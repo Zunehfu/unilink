cb9930b95425a73001e80ee65d2d19da38975153
GameOvr <denethbada@gmail.com>
2024-04-19 20:37:36 +0530

    same
    same

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const hbs = require('hbs');
+const hbs = require('hbs')
+const cookieParser = require('cookie-parser')
 
 dotenv.config({path: './config.env'}) //dotenv paths start from the root
 
@@ -11,14 +12,14 @@ const port = 5000
 app.set('view engine', 'hbs');
 hbs.localsAsTemplateData(app);
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use('/', require('./routes/router'))
 
 mongoose.connect(process.env.CONN_STRL, {
-    useNewUrlParser: true
+    // useNewUrlParser: true
 }).then((conn) => {
-    console.log(conn)
     console.log('DB connection successful')
 }).catch((err) => {
     console.log(err)
--- routes/router.js
+++ routes/router.js
@@ -1,26 +1,33 @@
 const express = require('express')
 const authController = require('../controllers/authController')
 const postController = require('../controllers/postController')
+const userController = require('../controllers/userController')
 
 const router = express.Router()
 
 router.route('/posts')
-    .get(authController.protect, postController.getAllPosts)
-    .post(postController.addPost, postController.addPostErrorHandler)
+    .get(authController.protectRoute, postController.getAllPosts)
+    .post(authController.protectRoute, postController.addPost, postController.addPostErrorHandler)
 
 router.route('/posts/:id')
-    .get(postController.getPostPage)
-    .post(postController.addToPostPage, postController.addToPostPageErrorHandler)
+    .get(authController.protectRoute, postController.getPostPage)
+    .post(authController.protectRoute, postController.addToPostPage, postController.addToPostPageErrorHandler)
+
+router.route('/')
+    .get((req, res) => { res.redirect('/signin') })
 
 router.route('/signin')
     .get(authController.getSigninPage)
     .post(authController.verifySignin)
 
-router.route('/')
-    .get((req, res) => {res.redirect('/signin')})
-
 router.route('/signup')
     .get((req, res) => {res.send('signup page')})
     .post(authController.verifySignup)
+
+router.route('/users/')
+    .get(authController.protectRoute, userController.getAllUsers)
+
+router.route('/users/:id')
+    .get(authController.protectRoute, userController.getUserWithId)
 
 module.exports = router
